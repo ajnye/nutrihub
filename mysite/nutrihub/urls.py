8da0d4dd3112168491f7e7a2eefd6c01307c93b8
cherylbai7 <cherylbai7@gmail.com>
2023-03-25 12:32:56 -0400

    Home page

---
+++ mysite/nutrihub/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'nutrihub'
+urlpatterns = [
+    path('', views.home_page, name='home')
+    # path('tutor/', include('tutor.urls')),
+    # path('student/', include('student.urls')),
+    # path('course/', views.tutor_search_courses, name='search'),
+    # path('detail/', views.course_detail, name='course-detail'),
+    # path('profile/', views.tutor_profile, name = 'tutor_profile')
+]
