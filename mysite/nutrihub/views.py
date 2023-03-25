5b582aee52b956128d7375358c9d5db0b45673cf
cherylbai7 <cherylbai7@gmail.com>
2023-03-25 12:46:51 -0400

    Fixing urls

--- mysite/nutrihub/views.py
+++ mysite/nutrihub/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import Http404
+from .models import *
+from django import forms
+from django.urls import reverse
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.contrib.auth import *
+from allauth.socialaccount import *
+import time
+import requests
 
 # Create your views here.
 
